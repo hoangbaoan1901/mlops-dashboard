0c9556df2f91c21364f083a2db06fc336453dede
Bao An Hoang <hoangbaoan1901@gmail.com>
2025-10-20 11:00:23 +0000

    configured backend to run

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,26 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import userRouter from "./user/userRouter";
+import workspaceRouter from "./workspace/workspaceRouter";
 
 const app = express();
 
+// Enable CORS
+app.use(
+    cors({
+        origin: ["http://localhost:3000", "http://localhost:5173"],
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    })
+);
+app.options("/", cors());
+
+// Routers
+app.use("/users", userRouter);
+app.use("/workspaces", workspaceRouter);
+
+app.listen(3000, function () {
+    console.log("App listening on port 3000");
+});
